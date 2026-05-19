a9082f39d63d909ef09c9adfd685dfc93f4735ff
gilbertsitumorang30 <gilbertsitumorang30@gmail.com>
2026-05-20 06:33:04 +0700

    add bash scripts

---
+++ k6/fastify/run_fastify_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+vusList=(10 50 100 250 500 1000)
+
+for vus in "${vusList[@]}"
+do
+    echo ""
+    echo "======================================="
+    echo "FASTIFY GRADES TEST - $vus VUs"
+    echo "======================================="
+
+    k6 run \
+        -e VUS=$vus \
+        -e DURATION=30s \
+        ./grades_test.js \
+        --summary-export=./results/grades_${vus}_summary.json
+
+    echo ""
+    echo "======================================="
+    echo "FASTIFY REGISTRATION TEST - $vus VUs"
+    echo "======================================="
+
+    k6 run \
+        -e VUS=$vus \
+        -e DURATION=30s \
+        ./registration_test.js \
+        --summary-export=./results/registration_${vus}_summary.json
+
+done
